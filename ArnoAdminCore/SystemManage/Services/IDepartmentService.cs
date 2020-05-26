7240c9025056000b0bd391bdba40196671ff026c
Arno Feng <853430390@qq.com>
2020-05-26 18:48:19 +0800

    dept

---
+++ ArnoAdminCore/SystemManage/Services/IDepartmentService.cs
@@ -0,0 +1,12 @@
+﻿using ArnoAdminCore.Base.Services;
+using ArnoAdminCore.SystemManage.Models.Poco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArnoAdminCore.SystemManage.Services
+{
+    public interface IDepartmentService : IBaseCRUDService<Department>
+    {
+    }
+}
---
+++ ArnoAdminCore/SystemManage/Services/Impl/DepartmentService.cs
@@ -0,0 +1,14 @@
+﻿using ArnoAdminCore.Base.Services.Impl;
+using ArnoAdminCore.SystemManage.Models.Poco;
+using ArnoAdminCore.SystemManage.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArnoAdminCore.SystemManage.Services.Impl
+{
+    public class DepartmentService : BaseCRUDService<Department>, IDepartmentService
+    {
+        public DepartmentService(DepartmentRepository repo) : base(repo) { }
+    }
+}
